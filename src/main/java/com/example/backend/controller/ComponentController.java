93f16e1f4e27be7db8a4e21c2469a1a8d6000085
human729 <gordeevglebanufa@gmail.com>
2025-03-10 15:10:49 +0500

    repository

--- src/main/java/com/example/backend/controller/ComponentController.java
+++ src/main/java/com/example/backend/controller/ComponentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/components")
 public class ComponentController {
     private ComponentService service;
 
